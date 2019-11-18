127e18478288b945b7d83c164107b1fc9be43259
YeongG <ohydung@naver.com>
2019-11-18 23:25:46 +0900

    비밀번호 확인 기능 구현

--- src/Component/Register/RegisterPassword.js
+++ src/Component/Register/RegisterPassword.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React,{Component,createRef} from 'react';
 import {Link} from 'react-router-dom';
 import {post} from 'axios';
 import Standard from '../Reuse/Standard';
@@ -12,6 +12,7 @@ class RegisterAccount extends Component {
             passwordCheck:''
         };
     }
+    error = createRef();
     componentWillMount(){
         if(!this.props.location.state) {
             alert("잘못된 접근 입니다."); 
@@ -27,11 +28,20 @@ class RegisterAccount extends Component {
             }
             console.log(submitValue);
             post("http://13.124.89.129/signup",submitValue);
+            
         }
-        e.preventDefault();
+        else {
+            e.preventDefault();
+        }
     }
     stateChange = e => {
         this.setState({[e.target.name]:e.target.value});
+    }
+    errorChange = () => {
+        if(this.state.password!==this.state.passwordCheck) {
+            this.error.current.classList.add("error");
+        }
+        else this.error.current.classList.remove("error");
     }
     render() {
         return (
@@ -41,9 +51,11 @@ class RegisterAccount extends Component {
                         <h1>비밀번호를 생성하세요.</h1>
                     </div>
                     <div className="password-body">
-                        <input name="password" onChange={this.stateChange} value={this.state.password} minLength="6" maxLength="12" placeholder="비밀번호" type="password" className="password-input" />
-                        <input name="passwordCheck" onChange={this.stateChange} value={this.state.passwordCheck} minLength="6" maxLength="12" placeholder="비밀번호 확인" type="password" className="password-input" /><br/><br/><br/><br/><br/><br/><br/>
-                        <Link onClick={this.register} className="next-btn" to="/register/" >회원가입</Link>
+                        <input name="password" onChange={this.stateChange} ref={this.input} value={this.state.password} minLength="6" maxLength="12" placeholder="비밀번호" type="password" className="password-input" />
+                        <input name="passwordCheck" onKeyUp={this.errorChange} onChange={this.stateChange} value={this.state.passwordCheck} minLength="6" maxLength="12" placeholder="비밀번호 확인" type="password" className="password-input" /><br/>
+                        <span style={{opacity:0}} ref={this.error}>비밀번호와 비밀번호 확인이 일치하지 않습니다</span>
+                        <br/><br/><br/><br/><br/><br/><br/>
+                        <Link onClick={this.register} className="next-btn" to="/register/success">회원가입</Link>
                     </div>
                 </div>
             </Standard>
